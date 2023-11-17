6a790fb69b64aba92f0aee70833baf9b1ef35220
Ryan Token <ryantoken13@gmail.com>
2023-11-17 10:06:58 -0700

    Initial Commit

---
+++ MetalSwiftUI/MetalSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalSwiftUIApp.swift
+//  MetalSwiftUI
+//
+//  Created by Ryan Token on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MetalSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
